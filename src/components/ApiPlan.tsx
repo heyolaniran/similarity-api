99b6adff8aae322a5f707568f2b6fc0e181eea6b
heyolaniran <olaniran.abd@gmail.com>
2023-04-07 12:20:33 +0100

    fix prisma model & plan components starting

---
+++ src/components/ApiPlan.tsx
@@ -0,0 +1,15 @@
+
+import {FC} from 'react' 
+
+interface ApiPlanProps {
+    
+}
+ 
+const ApiPlan: FC<ApiPlanProps> = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default ApiPlan;
